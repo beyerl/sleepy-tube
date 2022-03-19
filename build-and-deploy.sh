3536922ee231761ce3fcec11eeb501272ed2b577
Lorenz Beyer <lorenzbeyer@posteo.de>
2022-03-19 21:04:10 +0100

    alter build script, amend previous commit

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 ionic build --prod -- --base-href https://beyerl.github.io/sleepy-tube/
-git add -A &&
-git commit -m "build and deploy" &&
-git push &&
-git subtree push --prefix www origin gh-pages
+&& npx angular-cli-ghpages --dir=www
