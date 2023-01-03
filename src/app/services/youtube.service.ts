f3ce69003f66353bf91b92d94561ff3ff3dbde9a
Lorenz Beyer <lorenzbeyer@googlemail.com>
2023-01-03 13:56:26 +0100

    fix playlist instead of video search result bug

--- src/app/services/youtube.service.ts
+++ src/app/services/youtube.service.ts
@@ -12,16 +12,8 @@ export class YoutubeService {
 
   constructor(public http: HttpClient) { }
 
-  getVideosForChanel(channel, maxResults): Observable<Object> {
-    let url = 'https://www.googleapis.com/youtube/v3/search?key=' + this.apiKey + '&channelId=' + channel + '&order=date&part=snippet &type=video,id&maxResults=' + maxResults
-    return this.http.get(url)
-      .pipe(map((res) => {
-        return res;
-      }))
-  }
-
   search(searchTerm: string, maxResults: number) {
-    return this.http.get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${maxResults}&q=${searchTerm}&key=${this.apiKey}`).toPromise()
+    return this.http.get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&type=video&maxResults=${maxResults}&q=${searchTerm}&key=${this.apiKey}`).toPromise()
   }
 
   // eventuell wrapper verwenden? https://www.npmjs.com/package/youtube.ts
