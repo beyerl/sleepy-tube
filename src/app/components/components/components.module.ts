8037c60ee3348ae466c42f8a433a6c2a9817afe5
Lorenz Beyer <lorenzbeyer@googlemail.com>
2023-01-04 00:00:05 +0100

    build fixes

--- src/app/components/components/components.module.ts
+++ src/app/components/components/components.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VideoListComponent } from '../video-list/video-list.component';
+import { IonicModule } from '@ionic/angular';
 
 
 
 @NgModule({
   declarations: [VideoListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    IonicModule
   ],
   exports: [
     VideoListComponent
